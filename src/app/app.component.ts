810100efc61735845fdca9cc8556bc008060f4f4
Bharti-harinkhede <bhartiui@gmail.com>
2026-01-11 15:06:21 +0530

    ngInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdForms';
+
+  @ViewChild('signUpForm') signUpForm !: NgForm
+
+  userDetails = {
+    userName: 'iamJohnDoe',
+    email: 'johndoe@gmail.com',
+    secreteQuestion: 'favBook',
+    answer: 'The Alchemist',
+    modOfContact: 'phone'
+  }
+
+  modCon = [{
+    contactMode: 'phone',
+    id: 123
+  },
+  {
+    contactMode: 'email',
+    id: 124
+
+  }
+  ]
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm)
+      console.log(this.signUpForm.form.value)
+
+      this.signUpForm.reset()
+    }
+  }
+
+ patchData(){
+  this.signUpForm.form.patchValue(this.userDetails)
+ }
 }
+
